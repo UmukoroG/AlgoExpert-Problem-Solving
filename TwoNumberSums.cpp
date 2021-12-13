d8dd0c92bf15a9ee779350439e5d5436d7294005
DiromaUmukoro <godbless.umukoro@yahoo.com>
2021-12-12 23:37:52 -0600

    initial commit

--- TwoNumberSums.cpp
+++ TwoNumberSums.cpp
@@ -1,6 +1,7 @@
 //SOLUTION 1
 
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 vector<int> twoNumberSum(vector<int> array, int targetSum) {
@@ -22,5 +23,28 @@ vector<int> twoNumberSum(vector<int> array, int targetSum) {
 }
 
 // SOLUTION 2
+
+vector<int> twoNumberSum(vector<int> array, int targetSum) {
+  // Write your code here.
+	unordered_map<int, int> map;
+	vector<int> ans;
+	if(array.size()==0){
+		return {};
+	}
+	
+	for(int i=0; i<array.size();i++){
+		if(map.find(targetSum-array[i])!=map.end()){
+			ans.push_back(targetSum-array[i]);
+			ans.push_back(array[i]);
+			
+		}
+		else{
+			map[array[i]]=i;
+		}
+		
+	}
+	
+  return ans;
+}
 
 
